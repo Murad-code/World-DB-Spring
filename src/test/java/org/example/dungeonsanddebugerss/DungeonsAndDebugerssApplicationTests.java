0eb6864acaca60c0647be9ba9de49050e66ac6a4
Howard <howardchung04@yahoo.com.hk>
2024-05-09 12:18:01 +0100

    Added tests for findCountOfMostPopularLanguage
    Tests
    Country exists - 2 official language, return the most popular one, 1 official language but not the most popular still return official language
    Country don't exists returns -1
    Country don't have official language return -1

--- src/test/java/org/example/dungeonsanddebugerss/DungeonsAndDebugerssApplicationTests.java
+++ src/test/java/org/example/dungeonsanddebugerss/DungeonsAndDebugerssApplicationTests.java
@@ -1,13 +1,50 @@
 package org.example.dungeonsanddebugerss;
 
+import org.example.dungeonsanddebugerss.service.WorldService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DungeonsAndDebugerssApplicationTests {
 
+    @Autowired
+    private WorldService worldService;
+
     @Test
     void contextLoads() {
+    }
+
+    @Nested
+    class testingFindCountOfMostPopularLanguage{
+        @Test
+        @DisplayName("Given Afghanistan, the number of people who speak the most popular official language is 11905280 ")
+        void givenAfghanistanTheNumberOfPeopleWhoSpeakTheMostPopularOfficialLanguageIs11905280() {
+            Assertions.assertEquals(11905280,worldService.findCountOfMostPopularLanguage("Afghanistan"));
+        }
+
+        @Test
+        @DisplayName("Given a country doesn't exists, return negative 1")
+        void givenACountryDoesnTExistsReturnNegative1() {
+            Assertions.assertEquals(-1,worldService.findCountOfMostPopularLanguage("BABYLON"));
+        }
+
+        @Test
+        @DisplayName("Given Aruba, the number of people who speak the most popular official language is ")
+        void givenArubaTheNumberOfPeopleWhoSpeakTheMostPopularOfficialLanguageIs() {
+            Assertions.assertEquals(5459,worldService.findCountOfMostPopularLanguage("Aruba"));
+        }
+
+        @Test
+        @DisplayName("Given Angola, country has no official language so it should return -1")
+        void givenAngolaCountryHasNoOfficialLanguageSoItShouldReturn1() {
+            Assertions.assertEquals(-1,worldService.findCountOfMostPopularLanguage("Angola"));
+        }
+
+
     }
 
 }
