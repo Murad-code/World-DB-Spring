7047c079540439a316c9fef2ee4530a2da4d277e
Phoenix-Sparta <57190209+RayFFH@users.noreply.github.com>
2024-05-09 11:40:22 +0100

    method for finding 5 smallest cities

--- src/main/java/org/example/dungeonsanddebugerss/service/WorldService.java
+++ src/main/java/org/example/dungeonsanddebugerss/service/WorldService.java
@@ -2,20 +2,21 @@ package org.example.dungeonsanddebugerss.service;
 
 import org.example.dungeonsanddebugerss.entities.CityEntity;
 import org.example.dungeonsanddebugerss.entities.CountryEntity;
-import org.example.dungeonsanddebugerss.entities.CountrylanguageEntity;
 import org.example.dungeonsanddebugerss.respositories.CityEntityRepository;
 import org.example.dungeonsanddebugerss.respositories.CountryEntityRepository;
 import org.example.dungeonsanddebugerss.respositories.CountrylanguageEntityRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 
 @Service
 public class WorldService {
 
-    private CityEntityRepository cityEntityRepository;
+    private static CityEntityRepository cityEntityRepository;
     private CountryEntityRepository countryEntityRepository;
     private CountrylanguageEntityRepository countrylanguageEntityRepository;
 
@@ -26,30 +27,54 @@ public class WorldService {
         this.countrylanguageEntityRepository = countrylanguageEntityRepository;
     }
 
-    public List<CountryEntity> findCountriesWithNoHeadOfState() {
+//    public List<CountryEntity> findCountriesWithNoHeadOfState() {
+//
+//    }
+//
+//
+//    public float findPercentageOfPopulationInLargestCity(String countryName) {
+//
+//    }
+//
+//    public List<CountryEntity> findCountryWithMostCity() {
+//
+//    }
+//
+//    public int returnCountOfCitiesInCountry() {
+//
+//    }
 
+    public static List<CityEntity> find5SmallestDistrictsOfCity(String city) {
+        List<CityEntity> cityEntities = cityEntityRepository.findAll();
+        ArrayList<CityEntity> lowest = new ArrayList<>();
+
+        for(CityEntity cityEntity : cityEntities) {
+            if(cityEntity.getDistrict().equals(city)){
+                lowest.add(cityEntity);
+            }
+        }
+        Comparator<CityEntity> populationComparator = new Comparator<CityEntity>() {
+            @Override
+            public int compare(CityEntity city1, CityEntity city2) {
+                return Integer.compare(city1.getPopulation(), city2.getPopulation());
+            }
+        };
+
+        Collections.sort(cityEntities, populationComparator);
+
+        List<CityEntity> top5Cities = cityEntities.subList(0, Math.min(5, cityEntities.size()));
+
+        for (int i = 0; i < top5Cities.size(); i++) {
+            CityEntity cities = top5Cities.get(i);
+            System.out.println("City " + (i+1) + ": " + cities.getName() + ", Population: " + cities.getPopulation());
+        }
+
+        return top5Cities;
     }
 
-
-    public float findPercentageOfPopulationInLargestCity(String countryName) {
-
-    }
-
-    public List<CountryEntity> findCountryWithMostCity() {
-
-    }
-
-    public int returnCountOfCitiesInCountry() {
-
-    }
-
-    public List<CityEntity> find5SmallestDistrictsOfCity() {
-
-    }
-
-    public int findCountOfMostPopularLanguage(String countryName) {
-
-    }
+//    public int findCountOfMostPopularLanguage(String countryName) {
+//
+//    }
 
 
 
