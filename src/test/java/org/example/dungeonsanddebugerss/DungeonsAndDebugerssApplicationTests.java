cd253918a354c0e670b7f2c7b180d72339cd7560
PatrykSysa <Psysa@spartaglobal.com>
2024-05-09 11:44:22 +0100

    Added method to WorldService to find countries with no head of state, with testing

--- src/test/java/org/example/dungeonsanddebugerss/DungeonsAndDebugerssApplicationTests.java
+++ src/test/java/org/example/dungeonsanddebugerss/DungeonsAndDebugerssApplicationTests.java
@@ -1,13 +1,32 @@
 package org.example.dungeonsanddebugerss;
 
+import org.example.dungeonsanddebugerss.service.WorldService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DungeonsAndDebugerssApplicationTests {
 
+    @Autowired
+    private WorldService worldService;
+
     @Test
     void contextLoads() {
     }
+
+
+    @Test
+    void checkCountryWithNoHeadOfStateIsCalledSanMarino() {
+        Assertions.assertEquals("San Marino", worldService.findCountriesWithNoHeadOfState().getFirst().getName());
+    }
+
+    @Test
+    void checkCountryWithNoHeadOfStateIsNotMoreThanOne() {
+        Assertions.assertEquals(1, worldService.findCountriesWithNoHeadOfState().size());
+    }
+
+
 
 }
